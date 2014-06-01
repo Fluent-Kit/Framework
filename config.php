22ab040c898dd7fec34c4c8ae02ee5e6b5e030e2
Lee Mason <contact@nohalfpixels.com>
2014-06-01 13:29:30 +0100

    ...

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+// Define the writable storage path
+define('STORAGE_PATH', __DIR__ . '/storage' );
+
+// Define the site url
+define('URL', 'http://dev.fluentkit.io' );
+
+// Define the site secret key
+define('SECRET_KEY', 'YourSecretKey!!!' );
+
+// DB details
+define('DB_HOST', 'localhost' );
+define('DB_NAME', 'fluentkit' );
+define('DB_USER', 'root' );
+define('DB_PASS', '' );
+define('DB_PREFIX', 'prefix_' );
+
+
+// Define debug true or false
+define('DEBUG', true );
